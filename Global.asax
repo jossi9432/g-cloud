7db7dbd0f8aa4ce5d0743c1bddb2bf588027b894
jossi9432 <jossi9432@gmail.com>
2019-08-06 01:24:06 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="g_cloud.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(g_cloud.Startup))]
+namespace g_cloud
+{
+    public partial class Startup
+    {/// <summary>
+    /// 
+    /// </summary>
+    /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
